f104058ff96203aa2a0b22b09327c5ef977fb431
Felix <broadcastzero@kaffeeschluerfer.com>
2012-03-11 12:57:59 +0100

    Rewrites class DataBaseConnector.

---
+++ EPU-Backoffice/EPU-Backoffice/BL/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using EPUBackoffice.Dal;
+
+namespace EPUBackoffice
+{
+    /// <summary>
+    /// The main entry class of the application.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// Main entry point of the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /***
+             * Inplements error logging.
+             * Debug.WriteLines will be stored in logfile only in Debug-Mode.
+             * Trace.WriteLines will be stored in logfile in Debug AND Release Mode.
+             * see http://support.microsoft.com/kb/815788 for further information.
+             */
+            TextWriterTraceListener tr1 = new TextWriterTraceListener(System.IO.File.CreateText("Logfile.txt"));
+            Trace.Listeners.Add(tr1);
+            Trace.AutoFlush = true;
+
+            /***
+             * Checks data base existance and creates if needed.
+             */
+            DataBaseConnector dbc = new DataBaseConnector();
+            bool exists = dbc.checkDataBaseExistance();
+            if (!exists)
+            {
+                // show info-form and ask for user input
+                // ask user for path
+                dbc.checkDataBaseExistance("db.db");
+                // give path or create new? -> input window
+                // do the following in an eventhandler!!
+                //create new database (if user has not given path)
+                bool create = false;
+                if (create)
+                {
+                    dbc.createDataBase();
+                }
+            }
+            else // database found. Start with home screen.
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
---
+++ EPU-Backoffice/EPU-Backoffice/GUI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EPUBackoffice
+{
+    /// <summary>
+    /// Class of main window
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Form of first window
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
