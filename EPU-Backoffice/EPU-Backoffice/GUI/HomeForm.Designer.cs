d7dc7a2b441cad70220a7eac78b348f4ed9412bb
Marvin Vesely <marvin_vesely@hotmail.com>
2012-03-28 14:07:50 +0200

    update gui forms

--- EPU-Backoffice/EPU-Backoffice/GUI/HomeForm.Designer.cs
+++ EPU-Backoffice/EPU-Backoffice/GUI/HomeForm.Designer.cs
@@ -28,36 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelKunden = new System.Windows.Forms.Panel();
+            this.tabKundenKontakte = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panelKunden.SuspendLayout();
+            this.tabKundenKontakte.SuspendLayout();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // button8
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(395, 340);
-            this.splitContainer1.SplitterDistance = 131;
-            this.splitContainer1.TabIndex = 0;
+            this.button8.Location = new System.Drawing.Point(12, 213);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(126, 23);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Beenden";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 184);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(126, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Reports";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 155);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(126, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Zeiterfassung";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Projektverwaltung";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Angebote";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Rechnungsverwaltung";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Kunden und Kontakte";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panelKunden
+            // 
+            this.panelKunden.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelKunden.Controls.Add(this.tabKundenKontakte);
+            this.panelKunden.Location = new System.Drawing.Point(152, 12);
+            this.panelKunden.Name = "panelKunden";
+            this.panelKunden.Size = new System.Drawing.Size(770, 238);
+            this.panelKunden.TabIndex = 16;
+            this.panelKunden.Visible = false;
+            this.panelKunden.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // tabKundenKontakte
+            // 
+            this.tabKundenKontakte.Controls.Add(this.tabPage1);
+            this.tabKundenKontakte.Controls.Add(this.tabPage2);
+            this.tabKundenKontakte.Location = new System.Drawing.Point(-1, -1);
+            this.tabKundenKontakte.Name = "tabKundenKontakte";
+            this.tabKundenKontakte.SelectedIndex = 0;
+            this.tabKundenKontakte.Size = new System.Drawing.Size(770, 238);
+            this.tabKundenKontakte.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(762, 212);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Neu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(762, 212);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Suchen & Aendern";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 340);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(934, 262);
+            this.Controls.Add(this.panelKunden);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "HomeForm";
-            this.Text = "HomeForm";
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.Text = "EPU Backoffice 1.0";
+            this.Load += new System.EventHandler(this.test_Load);
+            this.panelKunden.ResumeLayout(false);
+            this.tabKundenKontakte.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelKunden;
+        private System.Windows.Forms.TabControl tabKundenKontakte;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+
+
+
     }
 }
--- EPU-Backoffice/EPU-Backoffice/GUI/HomeForm.cs
+++ EPU-Backoffice/EPU-Backoffice/GUI/HomeForm.cs
@@ -21,5 +21,56 @@ namespace EPUBackoffice.Gui
         {
             InitializeComponent();
         }
+
+        private void test_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+        
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
