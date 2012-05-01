96f169aaaf13122be53beb49766a6eafcb0b4654
Felix <broadcastzero@kaffeeschluerfer.com>
2012-05-01 09:29:08 +0200

    Re-adds some unit tests

---
+++ EPU-Backoffice/EPUBackofficeTests/ConfigFileManagerTest.cs
@@ -0,0 +1,116 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ConfigFileManagerTest.cs" company="Marvin&Felix">
+// You can use the source code just as you wish. Exception: do not copy the whole or parts of this file, 
+// if you also have to submit this homework.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BackofficeTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+    using EPU_Backoffice_Panels.BL;
+    using EPU_Backoffice_Panels.Dal;
+
+    /// <summary>
+    ///This is a test class for ConfigFileManager and is intended
+    ///to contain all ConfigFileManager Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ConfigFileManagerTest
+    {
+        private TestContext testContextInstance;
+        private ConfigFileManager target;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //    
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            target = new ConfigFileManager();
+        }
+        //
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            target = null;
+        }
+        //
+        #endregion
+
+        /// <summary>
+        ///A test for checkDataBaseExistance with a string as parameter
+        ///</summary>
+        [TestMethod()]
+        public void CheckDataBaseExistanceTest1()
+        {
+            string path = "../../../EPU-Backoffice/bin/Debug/backoffice_database.db"; // create this file in test folder!
+            bool expected = true;
+            bool actual;
+            actual = target.CheckDataBaseExistance(path);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// A test for checkDataBaseExistance with a string as parameter - file does not end with .db
+        /// Should return false, because "Logfile.txt" does exist, but does not end with ".db".
+        /// </summary>
+        [TestMethod()]
+        public void CheckDataBaseExistanceTest2()
+        {
+            string path = "../../../EPU-Backoffice/bin/Debug/logs.txt";
+            bool expected = false;
+            bool actual;
+            actual = target.CheckDataBaseExistance(path);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for checkDataBaseExistance without params - path is read out of Logfile.
+        ///Actual should be false, when no Logfile is found (which is the case within the path in which tests are run).
+        ///</summary>
+        [TestMethod()]
+        public void CheckLogfileExistanceTest()
+        {
+            bool expected = false;
+            bool actual;
+            actual = target.CheckDataBaseExistance();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ EPU-Backoffice/EPUBackofficeTests/DataBaseManagerTest.cs
@@ -0,0 +1,143 @@
+﻿namespace BackofficeTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using EPU_Backoffice_Panels.Dal;
+    using EPU_Backoffice_Panels.Dal.Tables;
+
+    /// <summary>
+    ///This is a test class for DataBaseManagerTest and is intended
+    ///to contain all DataBaseManagerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DataBaseManagerTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetKundenKontakteSQL
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EPU-Backoffice.exe")]
+        public void GetKundenKontakteSQLTest()
+        {
+            DataBaseManager target = new DataBaseManager();
+
+            KundeKontaktTable k = new KundeKontaktTable();
+            k.ID = -1; // indicates that it shall not be searched for ID
+            k.Type = false;
+            k.Vorname = string.Empty;
+            k.NachnameFirmenname = "Huber";
+            string expected = "SELECT * FROM Kunde WHERE Nachname_Firmenname = ?";
+            string actual;
+            actual = target.GetKundenKontakteSQL(k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A second test for GetKundenKontakteSQL
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EPU-Backoffice.exe")]
+        public void GetKundenKontakteSQLTest1()
+        {
+            DataBaseManager target = new DataBaseManager();
+
+            KundeKontaktTable k = new KundeKontaktTable();
+            k.ID = -1; // indicates that it shall not be searched for ID
+            k.Type = false;
+            k.Vorname = "Franz";
+            k.NachnameFirmenname = string.Empty;
+            string expected = "SELECT * FROM Kunde WHERE Vorname = ?";
+            string actual;
+            actual = target.GetKundenKontakteSQL(k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A third test for GetKundenKontakteSQL
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EPU-Backoffice.exe")]
+        public void GetKundenKontakteSQLTest2()
+        {
+            DataBaseManager target = new DataBaseManager();
+
+            KundeKontaktTable k = new KundeKontaktTable();
+            k.ID = -1; // indicates that it shall not be searched for ID
+            k.Type = false;
+            k.Vorname = string.Empty;
+            k.NachnameFirmenname = string.Empty;
+            string expected = "SELECT * FROM Kunde"; ;
+            string actual;
+            actual = target.GetKundenKontakteSQL(k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A fourth test for GetKundenKontakteSQL
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EPU-Backoffice.exe")]
+        public void GetKundenKontakteSQLTest3()
+        {
+            DataBaseManager target = new DataBaseManager();
+            KundeKontaktTable k = new KundeKontaktTable();
+            k.ID = -1; // don't search for ID
+            k.Type = true;
+            k.Vorname = "Hans";
+            k.NachnameFirmenname = "Huber";
+            string expected = "SELECT * FROM Kontakt WHERE Vorname = ? AND Nachname_Firmenname = ?";
+            string actual;
+            actual = target.GetKundenKontakteSQL(k);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
